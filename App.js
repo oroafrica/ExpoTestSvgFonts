b0fd82ffdd347962d210a2590bceda000b028e69
Mikael Sand <msand@abo.fi>
2019-08-08 23:35:34 +0300

    Add repro of expo-fonts ios issue #606

--- App.js
+++ App.js
@@ -1,38 +1,61 @@
 import React, { Component } from 'react';
-import { Platform, StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View, Text } from 'react-native';
+import { Svg, G, Text as SVGText } from 'react-native-svg';
+import * as Font from 'expo-font';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' + 'Shake or press menu button for dev menu',
-});
+StyleSheet.setStyleAttributePreprocessor('fontFamily', Font.processFontFamily);
 
 export default class App extends Component {
+  state = { fontLoaded: false };
+  async componentDidMount() {
+    await Font.loadAsync({
+      IBMPlexSansVar: require('./assets/fonts/IBMPlexSansVar.ttf'),
+      'PublicSans-Thin_Regular': require('./assets/fonts/PublicSans-Thin_Regular.ttf'),
+    });
+    this.setState({ fontLoaded: true });
+  }
   render() {
-    return (
+    return this.state.fontLoaded ? (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+        <Svg {...{
+          fill: '#000',
+          height: 200,
+          width: '100%',
+          viewBox: '-140 0 280 120',
+        }}>
+          <G fontSize={40} textAnchor="middle">
+            <SVGText x={0} y={40} fontFamily="PublicSans-Thin_Regular">
+              Hello, world!
+            </SVGText>
+            <SVGText x={0} y={80} fontFamily="IBMPlexSansVar">
+              Hello, world!
+            </SVGText>
+            <SVGText x={0} y={120}>
+              Hello, world!
+            </SVGText>
+          </G>
+        </Svg>
+        <Text style={[styles.text, { fontFamily: 'PublicSans-Thin_Regular' }]}>
+          Hello, world!
+        </Text>
+        <Text style={[styles.text, { fontFamily: 'IBMPlexSansVar' }]}>
+          Hello, world!
+        </Text>
+        <Text style={styles.text}>Hello, world!</Text>
       </View>
-    );
+    ) : null;
   }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
     alignItems: 'center',
+    justifyContent: 'center',
     backgroundColor: '#F5FCFF',
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+  text: {
+    fontSize: 56,
+    color: '#000',
+  }
 });
